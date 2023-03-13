55d55d86bbe5cd1ca1b1e10fb1c211e319fe3ff4
Shawn Koong <shawnkoong@gmail.com>
2023-03-13 16:42:29 -0700

    initial commit

---
+++ backend/ChinguBoard/src/main/java/com/chingu/ChinguBoard/ChinguBoardApplication.java
@@ -0,0 +1,13 @@
+package com.chingu.ChinguBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChinguBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChinguBoardApplication.class, args);
+	}
+
+}
---
+++ backend/ChinguBoard/src/test/java/com/chingu/ChinguBoard/ChinguBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chingu.ChinguBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChinguBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
