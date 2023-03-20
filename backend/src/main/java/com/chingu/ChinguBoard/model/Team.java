bdd84c59cbf2c5df1597d0e4fa6e6762ea7c7b54
Shawn Koong <shawnkoong@gmail.com>
2023-03-20 13:06:11 -0700

    adding some comments to Team

--- backend/src/main/java/com/chingu/ChinguBoard/model/Team.java
+++ backend/src/main/java/com/chingu/ChinguBoard/model/Team.java
@@ -16,6 +16,12 @@ public class Team {
 
     private List<User> members;
 
+    /**
+     * think it is best for Team to have a list of Project IDs instead of the actual object
+     * example workflow:
+     * when user first gets to the board, the client only gets list of all teams (GET request to `/api/teams`) and display all current teams (maybe add a field of List<String> teamNames to be able to display team names without calling teams API)
+     * when user clicks on a team, client sends GET request to `/api/teans/{id}` to get the Team object, which would 
+     */
     private List<Project> projects;
 
     public Team() {
