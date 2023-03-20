e5dd1305229c86b756015cd7b07d1a7972d7703a
BillyMiles <williammiles201@gmail.com>
2023-03-20 17:34:42 -0400

    change branch to development

--- frontend-vite/src/App.jsx
+++ frontend-vite/src/App.jsx
@@ -2,7 +2,7 @@
 function App() {
 
   return (
-    <div className="w-screen h-screen bg-red-300">
+    <div >
         Hello
     </div>
   )
--- frontend-vite/src/main.jsx
+++ frontend-vite/src/main.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { Auth0Provider } from '@auth0/auth0-react'
+// import { Auth0Provider } from '@auth0/auth0-react'
 
-const domain = process.env.REACT_APP_AUTH0_DOMAIN
-const clientId = process.env.REACT_APP_CLIENT_ID
+// const domain = process.env.REACT_APP_AUTH0_DOMAIN
+// const clientId = process.env.REACT_APP_CLIENT_ID
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Auth0Provider
+    {/* <Auth0Provider
       domain={domain}
       clientId={clientId}
       redirectUri={windown.location.origin}
-    > 
-    
+    >  */}
+
       <App />
-    </Auth0Provider>
+    {/* </Auth0Provider> */}
   </React.StrictMode>,
 )
