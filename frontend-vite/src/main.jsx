a02f32b82deb1ead544c7fdd132f3c9e1f60defc
BillyMiles <williammiles201@gmail.com>
2023-03-21 19:53:42 -0400

    redirect uri syntax edited

--- frontend-vite/src/main.jsx
+++ frontend-vite/src/main.jsx
@@ -6,14 +6,14 @@ import { Auth0Provider } from '@auth0/auth0-react'
 
 const domain = import.meta.env.VITE_AUTH0_DOMAIN
 const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID
+const redirectUri = window.location.origin
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Auth0Provider 
-       domain={domain}
-       clientId={clientId}
-       redirect_uri={window.location.origin}
-
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      redirectUri={redirectUri}
     >
       <App />
     </Auth0Provider>
