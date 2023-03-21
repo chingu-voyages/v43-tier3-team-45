563e9084eebdc1ff722980c86902e8e984883d96
BillyMiles <williammiles201@gmail.com>
2023-03-21 13:13:29 -0400

    vite config file added auth0 domain, client

--- frontend-vite/src/main.jsx
+++ frontend-vite/src/main.jsx
@@ -4,17 +4,9 @@ import App from './App'
 import './index.css'
 import { Auth0Provider } from '@auth0/auth0-react'
 
-const domain = process.env.REACT_APP_AUTH0_DOMAIN
-const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Auth0Provider 
-        domain={domain}
-        clientId={clientId}
-        redirectUri={window.location.origin}
-      >
+    <Auth0Provider >
       <App />
     </Auth0Provider>
   </React.StrictMode>,
