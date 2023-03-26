21083e698ea9d56561edd608494085d3909a13e9
Shawn Koong <shawnkoong@gmail.com>
2023-03-25 22:55:46 -0700

    axios + redux for auth

--- frontend-vite/src/main.jsx
+++ frontend-vite/src/main.jsx
@@ -1,25 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { Auth0Provider } from '@auth0/auth0-react'
-import { Provider } from 'react-redux';
-import { store }from './store/store'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import { BrowserRouter } from "react-router-dom";
 
-const domain = import.meta.env.VITE_AUTH0_DOMAIN
-const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID
-const redirectUri = window.location.origin
+const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+const redirectUri = window.location.origin;
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Auth0Provider
       domain={domain}
       clientId={clientId}
       redirectUri={redirectUri}
-      >
-      <Provider store={store} >
-        <App />
+    >
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </Provider>
     </Auth0Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- frontend-vite/src/util/AxiosInstance.js
+++ frontend-vite/src/util/AxiosInstance.js
@@ -1,1 +1,25 @@
 import axios from "axios";
+import store from "../store/store";
+
+// change for deployment
+const BASE_URL = "http://localhost:8080/api";
+
+const axiosInstance = axios.create({
+    baseURL: BASE_URL,
+});
+
+// setting axios interceptor to include JWT for every request
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const token = store.getState().auth.token;
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export default axiosInstance;
