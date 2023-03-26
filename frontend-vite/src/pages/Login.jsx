a21d42a8c587b6c6c004fe8f55847b9f65eb66b1
BillyMiles <williammiles201@gmail.com>
2023-03-26 11:54:56 -0400

    login page init

---
+++ frontend-vite/src/pages/Login.jsx
@@ -0,0 +1,14 @@
+import LoginButton from "../components/LoginButton"
+import LogoutButton from "../components/LogoutButton"
+
+function Login () {
+
+    return (
+        <div>
+
+        </div>
+    )
+
+}
+
+export default Login 
