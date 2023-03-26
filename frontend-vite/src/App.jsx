132f0fa14bf747b2f8d0545d619781a498b71981
BillyMiles <williammiles201@gmail.com>
2023-03-26 11:30:40 -0400

    git branch snafu

--- frontend-vite/src/App.jsx
+++ frontend-vite/src/App.jsx
@@ -1,14 +1,10 @@
 import './App.css'
-import LoginButton from './components/LoginButton';
-import LogoutButton from './components/LogoutButton';
 
 function App() {
 
   return (
-    <div>
-      <h1>Sign In</h1>
-      <LoginButton />
-      <LogoutButton />
+    <div className="App">
+      
     </div>
   );
 }
