5a03e88249135e17cb8afe5b08fe310ce241c17c
Anna Harder <annaqharder@gmail.com>
2023-03-27 16:27:09 -0600

    commit before merging w development

--- frontend-vite/src/pages/Kanban.jsx
+++ frontend-vite/src/pages/Kanban.jsx
@@ -1,3 +1,89 @@
+// import React, {useState, useEffect} from 'react';
+// import { DragDropContext } from "react-beautiful-dnd";
+// import Column from "../components/Column"
+
+
+// export default function Kanban() {
+//     const [completed, setCompleted] = useState([]);
+//     const [incomplete, setIncomplete] = useState([]);
+//     const [backlog, setBacklog] = useState([])
+
+//     useEffect(() => {
+//         fetch("https://jsonplaceholder.typicode.com/todos")
+//             .then((r) => r.json())
+//             .then((json) => {
+//                 setCompleted(json.filter((task) => task.completed));
+//                 // setCompleted(json.filter((task) => task.column === 'completed'));
+//                 setIncomplete(json.filter((task) => !task.completed));
+//                 // setIncomplete(json.filter((task) => task.column === 'in-progress'));
+//                 // setBacklog(json.filter((task) => task.column === 'backlog'));
+//             })
+//     }, [])
+
+//     function findItemById(id, array) {
+//         return array.find((item) => item.id == id)
+//     }
+
+//     function removeItemById(id, array) {
+//         return array.filter((item) => item.id != id)
+//     }
+
+//     function handleDragEnd(result) {
+//         const { destination, source, draggableId} = result;
+
+//         if (source.droppableId == destination.droppableId) return;
+
+//         //REMOVE FROM SOURCE ARRAY
+
+//         if (source.droppableId == 2 ) {
+//             setCompleted(removeItemById(draggableId, completed));
+//         } else {
+//             setIncomplete(removeItemById(draggableId, incomplete))
+//         }
+
+//         // GET ITEM
+//         const task = findItemById(draggableId, [...incomplete, ...completed])
+
+//         // ADD ITEM
+//         if (destination.droppableId == 2 ) {
+//             setCompleted([{...task, completed: !task.completed}, ...completed]);
+//         } else {
+//             setIncomplete([{...task, completed: !task.completed}, ...incomplete])
+//         }
+//     }
+
+//     return (
+//         <DragDropContext onDragEnd={handleDragEnd}>
+//             <div class="flex">
+//                 <div class="grid grid-cols-4 gap-8">
+//                 <Column
+//                     title={"TO DO"}
+//                     tasks={incomplete}
+//                     id={"1"}
+//                 />
+//                 <Column
+//                     title={"COMPLETED"}
+//                     tasks={completed}
+//                     id={"2"}
+//                 />
+//                 <Column
+//                     title={"BACKLOG"}
+//                     tasks={[]}
+//                     id={"3"}
+//                 />
+//                 <Column
+//                     title={"IN PROGRESS"}
+//                     tasks={[]}
+//                     id={"4"}
+//                 />
+//                 </div>
+//             </div>
+
+//         </DragDropContext>
+//     )
+// }
+
+
 import React, {useState, useEffect} from 'react';
 import { DragDropContext } from "react-beautiful-dnd";
 import Column from "../components/Column"
@@ -55,7 +141,7 @@ export default function Kanban() {
     return (
         <DragDropContext onDragEnd={handleDragEnd}>
             <div class="flex">
-                <div class="grid grid-cols-3 gap-8">
+                <div class="grid grid-cols-4 gap-8">
                 <Column
                     title={"TO DO"}
                     tasks={incomplete}
@@ -71,11 +157,17 @@ export default function Kanban() {
                     tasks={[]}
                     id={"3"}
                 />
+                <Column
+                    title={"IN PROGRESS"}
+                    tasks={[]}
+                    id={"4"}
+                />
                 </div>
             </div>
-
         </DragDropContext>
     )
 }
+
+
 
 
