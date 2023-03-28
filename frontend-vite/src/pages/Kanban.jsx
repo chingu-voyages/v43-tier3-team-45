3fb5d131404a411c0699c52effab15f0ff38baca
Anna Harder <annaqharder@gmail.com>
2023-03-28 11:02:00 -0600

    tasks show up on board in correct columns

--- frontend-vite/src/pages/Kanban.jsx
+++ frontend-vite/src/pages/Kanban.jsx
@@ -86,25 +86,33 @@
 
 import React, {useState, useEffect} from 'react';
 import { DragDropContext } from "react-beautiful-dnd";
-import Column from "../components/Column"
+import Column from "../components/Column";
 
 
 export default function Kanban() {
+    // const [completed, setCompleted] = useState([]);
+    // const [incomplete, setIncomplete] = useState([]);
+
+    const [backlog, setBacklog] = useState([]);
+    const [newStatus, setNewStatus] = useState([]);
+    const [inProgress, setinProgress] = useState([]);
     const [completed, setCompleted] = useState([]);
-    const [incomplete, setIncomplete] = useState([]);
-    const [backlog, setBacklog] = useState([])
+
+
+    const [projects, setProjects] = useState({})
+
 
     useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/todos")
+        fetch("http://localhost:8080/api/projects/641ba8e494ba927d1a1e932d")
             .then((r) => r.json())
             .then((json) => {
-                setCompleted(json.filter((task) => task.completed));
-                // setCompleted(json.filter((task) => task.column === 'completed'));
-                setIncomplete(json.filter((task) => !task.completed));
-                // setIncomplete(json.filter((task) => task.column === 'in-progress'));
-                // setBacklog(json.filter((task) => task.column === 'backlog'));
+                setBacklog(json.issues.filter((issue) => issue.status == 'BACKLOG'))
+                setNewStatus(json.issues.filter((issue) => issue.status == 'NEW'))
+                setinProgress(json.issues.filter((issue) => issue.status == 'IN_PROGRESS'))
+                setCompleted(json.issues.filter((issue) => issue.status == 'DONE'))
             })
-    }, [])
+        }, [])
+
 
     function findItemById(id, array) {
         return array.find((item) => item.id == id)
@@ -124,42 +132,43 @@ export default function Kanban() {
         if (source.droppableId == 2 ) {
             setCompleted(removeItemById(draggableId, completed));
         } else {
-            setIncomplete(removeItemById(draggableId, incomplete))
+            setNewStatus(removeItemById(draggableId, newStatus))
         }
 
         // GET ITEM
-        const task = findItemById(draggableId, [...incomplete, ...completed])
+        const task = findItemById(draggableId, [...newStatus, ...completed])
 
         // ADD ITEM
         if (destination.droppableId == 2 ) {
             setCompleted([{...task, completed: !task.completed}, ...completed]);
         } else {
-            setIncomplete([{...task, completed: !task.completed}, ...incomplete])
+            setNewStatus([{...task, completed: !task.completed}, ...newStatus])
         }
     }
+
 
     return (
         <DragDropContext onDragEnd={handleDragEnd}>
             <div class="flex">
                 <div class="grid grid-cols-4 gap-8">
                 <Column
-                    title={"TO DO"}
-                    tasks={incomplete}
+                    title={"BACKLOG"}
+                    tasks={backlog}
                     id={"1"}
+                />
+                <Column
+                    title={"TO DO"}
+                    tasks={newStatus}
+                    id={"2"}
+                />
+                <Column
+                    title={"IN PROGRESS"}
+                    tasks={inProgress}
+                    id={"3"}
                 />
                 <Column
                     title={"COMPLETED"}
                     tasks={completed}
-                    id={"2"}
-                />
-                <Column
-                    title={"BACKLOG"}
-                    tasks={[]}
-                    id={"3"}
-                />
-                <Column
-                    title={"IN PROGRESS"}
-                    tasks={[]}
                     id={"4"}
                 />
                 </div>
