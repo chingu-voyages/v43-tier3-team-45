21d89b579aab09b70cc85d38a0a831d1c40b9249
talarsen <talarsen74@gmail.com>
2023-03-29 10:02:13 -0700

    profile page rendering data and css working

--- frontend-vite/src/pages/Profile.jsx
+++ frontend-vite/src/pages/Profile.jsx
@@ -1,51 +1,56 @@
 import React, { useState, useEffect } from "react";
-import Avatar from '../components/Avatar';
-import UpdateProfile from './UpdateProfile';
-import axios from 'axios';
+import Avatar from "../components/Avatar";
+import UpdateProfile from "./UpdateProfile";
+import axios from "axios";
 
 const Profile = () => {
-    const [user, setUser] = useState(null);
-
-    const fetchUser = async () => {
-      const data = await axios.get("/api/users/641ba82f94ba927d1a1e932a");
-      console.log(data)
-    }
-
-    useEffect(() => {
-      fetchUser().then((response) => setUser(response.data))
-      // make sure to catch any error
-      .catch(console.error);
-    }, []);
+  const [user, setUser] = useState([])
   
-    //const { firstName, lastName, role, email } = user 
-    const [showUpdateProfile, setShowUpdateProfile] = useState(false);
+  const fetchUser = async () => {
+    const { data } = await axios.get(
+      "http://localhost:8080/api/users/641ba82f94ba927d1a1e932a"
+    );
+    setUser(data)
+  };
 
-    const avatarSrc="https://gravatar.com/avatar/29348169ecc5b8d01ac28beb2c5a4a79?s=400&d=robohash&r=x"
-    
-    const handleUpdateProfileClick = () => {
-      setShowUpdateProfile(true);
-      console.log("edit button clicked")
-    };
+  useEffect(() => {
+    fetchUser()
+  }, []);
+
+  const { firstName, lastName, role, email } = user;
+  const [showUpdateProfile, setShowUpdateProfile] = useState(false);
+
+  const avatarSrc =
+    "https://gravatar.com/avatar/29348169ecc5b8d01ac28beb2c5a4a79?s=400&d=robohash&r=x";
+
+  const handleUpdateProfileClick = () => {
+    setShowUpdateProfile(true);
+    console.log("edit button clicked");
+  };
   return (
     <>
-      <div className="items-center justify-center">
-          <Avatar className="w-12 h-12 rounded-full border-2 border-gray-400 overflow-hidden"
-            src={avatarSrc}
-            alt="avatar"
-          />
-          <h1 className="text-xl font-bold py-2">{firstName}</h1>
-          <h1 className="text-xl font-bold py-2">{lastName}</h1>
-          <p className="text-lg py-2">{role}</p>
+      <div className="flex flex-col items-center justify-center mt-10">
+        <Avatar
+          className="w-12 h-12 rounded-full border-2 border-gray-400 overflow-hidden"
+          src={avatarSrc}
+          alt="avatar"
+        />
+        <h1 className="text-xl font-bold py-2">{firstName}</h1>
+        <h1 className="text-xl font-bold py-2">{lastName}</h1>
+        <p className="text-lg py-2">{role}</p>
       </div>
       <hr />
-      <div>
+      <div className="flex flex-col items-center justify-center">
         <p className="text-lg py-2">{email}</p>
-      </div> 
-        <button className="bg-blue-400 hover:bg-blue-700 text-black hover:text-white font-bold py-2 px-4 rounded mr-2 mt-8" onClick={handleUpdateProfileClick}>
-          Edit Profile
-        </button>
-        {showUpdateProfile ? <UpdateProfile /> : null}
-        
+      
+      <button
+        className=" bg-blue-400 hover:bg-blue-700 text-black hover:text-white font-bold py-2 px-4 rounded mr-2 mt-8"
+        onClick={handleUpdateProfileClick}
+      >
+        Edit Profile
+      </button>
+      {showUpdateProfile ? <UpdateProfile /> : null}
+      </div>
     </>
   );
 };
