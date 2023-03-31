d1973732dedcf2539a5c3606f19a664f50dc644f
BillyMiles <williammiles201@gmail.com>
2023-03-31 11:22:34 -0400

    clean up

--- frontend-vite/src/App.jsx
+++ frontend-vite/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Routes } from "react-router-dom";
 import './App.css'
 import Login from './pages/Login';
+import CreateIssue from './pages/CreateIssue';
 
 function App() {
 
   return (
     <div className="App">
         <Login />
+        <CreateIssue />
     </div>
   );
 }
