bafaf07e738ebca0e25c2bcb433de08a269484b0
Anna Harder <annaqharder@gmail.com>
2023-03-31 10:44:48 -0600

    made SideNavBar.jsx component

--- frontend-vite/src/App.jsx
+++ frontend-vite/src/App.jsx
@@ -1,12 +1,15 @@
 import React, {useState, useEffect} from "react";
 import "./App.css";
 import Kanban from "./pages/Kanban";
+import { Route, Link } from 'react-router-dom';
+import SideNavBar from "./components/SideNavBar";
 
 function App() {
 
   return (
     <div>
       <Kanban/>
+      <SideNavBar />
     </div>
   );
 }
---
+++ frontend-vite/src/components/SideNavBar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function SideNavBar() {
+
+  return
+    <div>
+    </div>
+}
+
+export default SideNavBar;
