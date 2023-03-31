7ab50c3adf71c809e058e01d141fe4427f294b0e
BillyMiles <williammiles201@gmail.com>
2023-03-31 10:43:45 -0400

    switching branches

--- frontend-vite/src/components/LoginForm.jsx
+++ frontend-vite/src/components/LoginForm.jsx
@@ -40,8 +40,8 @@ const LoginForm = () => {
         async (creds) => {
           try {
             const response = await axiosInstance.post(`/auth/login`, creds);
-            console.log(response)
-            return response.data;
+            // console.log("token", response.data.token)
+            return response.data.token;
           } catch (error) {
           }
         }
