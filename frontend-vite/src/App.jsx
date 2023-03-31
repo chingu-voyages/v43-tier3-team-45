3485e1734b008048316cb11fef15d9cf7b0f6d53
Anna Harder <annaqharder@gmail.com>
2023-03-31 12:39:57 -0600

    side navbar v1 complete

--- frontend-vite/src/App.jsx
+++ frontend-vite/src/App.jsx
@@ -1,15 +1,25 @@
 import React, {useState, useEffect} from "react";
 import "./App.css";
+import { Route, Routes } from 'react-router-dom';
 import Kanban from "./pages/Kanban";
-import { Route, Link } from 'react-router-dom';
 import SideNavBar from "./components/SideNavBar";
+import Profile from './pages/Profile';
+import NavBar from "./components/NavBar";
 
 function App() {
 
   return (
     <div>
-      <Kanban/>
-      <SideNavBar />
+      <SideNavBar/>
+      <NavBar />
+        <Routes>
+          <Route
+            path='/profile'
+            element={<Profile/>}>
+          </Route>
+        </Routes>
+        <Kanban/>
+
     </div>
   );
 }
