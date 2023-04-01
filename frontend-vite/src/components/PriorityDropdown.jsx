9df5d0156429b45467bc22115f0531c50e3a3fea
BillyMiles <williammiles201@gmail.com>
2023-04-01 11:31:43 -0400

    event listener added to dropdowns

--- frontend-vite/src/components/PriorityDropdown.jsx
+++ frontend-vite/src/components/PriorityDropdown.jsx
@@ -1,13 +1,26 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { addPriority } from '../store/issueReducer'
 
 
 const PriorityDropdown = () => {
+
+    const dispatch = useDispatch()
+
+    const handlePriority = (e) => {
+        e.preventDefault()
+        dispatch(addPriority(e.target.value))
+    }
+
+
     return (
         <div className="relative w-full lg:max-w-sm">
-            <select className="w-half  p-2.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600">
+            <select className="w-half  p-2.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600" 
+                onChange={handlePriority}
+                >
                 <option>Priority:</option>
-                <option>Type 1</option>
-                <option>Type 2</option>
-                <option>Type 3</option>
+                <option> 1</option>
+                <option> 2</option>
+                <option> 3</option>
             </select>
         </div>
     );
