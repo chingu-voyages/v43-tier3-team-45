e53c0437cbc7f258e99e5a6387d3e72bb90d174d
Shawn Koong <shawnkoong@gmail.com>
2023-04-04 22:43:22 -0700

    changing @modelAttribute to @RequestBody for updating user without image

--- backend/src/main/java/com/chingu/ChinguBoard/controller/UserController.java
+++ backend/src/main/java/com/chingu/ChinguBoard/controller/UserController.java
@@ -5,9 +5,9 @@ import java.util.stream.Collectors;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -55,7 +55,7 @@ public class UserController {
      * @return newly updated UserDTO
      */
     @PatchMapping("/{id}")
-    public ResponseEntity<UserDTO> updateUser(@PathVariable String id, @ModelAttribute RegisterRequest request) {
+    public ResponseEntity<UserDTO> updateUser(@PathVariable String id, @RequestBody RegisterRequest request) {
         UserDTO userDTO = userDTOMapper.toDTO(userService.updateUser(request, id));
         return ResponseEntity.ok(userDTO);
     }
