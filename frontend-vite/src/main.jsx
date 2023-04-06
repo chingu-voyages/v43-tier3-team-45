ba6abe61b5fd97764f4157b186d13c6c3d920149
Shawn Koong <shawnkoong@gmail.com>
2023-04-06 14:00:00 -0700

    adding redux persist

--- frontend-vite/src/main.jsx
+++ frontend-vite/src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "./store/store";
 import { BrowserRouter } from "react-router-dom";
 import { injectStore } from "./util/AxiosInstance";
 
@@ -12,9 +13,12 @@ injectStore(store);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      {/* can add react component to loading to show while store is be rehydrated */}
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- frontend-vite/src/pages/Home.jsx
+++ frontend-vite/src/pages/Home.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getAllTeams } from "../util/apiCalls";
 
 const Home = () => {
+  const [teams, setTeams] = useState();
+
+  useEffect(() => {
+    setTeams(getAllTeams());
+  }, []);
+
   return <div>Home</div>;
 };
 
