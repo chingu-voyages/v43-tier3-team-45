b481f568f66b49320366f377a3d5ef9c17af0621
Shawn Koong <shawnkoong@gmail.com>
2023-04-06 16:19:07 -0700

    trying team dropdown

---
+++ frontend-vite/src/components/TeamDropdown.jsx
@@ -0,0 +1,65 @@
+import React, { Fragment, useState } from "react";
+import { Listbox, Transition } from "@headlessui/react";
+import { CheckIcon } from "@heroicons/react/20/solid";
+import { useDispatch, useSelector } from "react-redux";
+import { setTeam } from "../store/teamReducer";
+
+const TeamDropdown = ({ teams }) => {
+  const dispatch = useDispatch();
+  const selectedTeam = useSelector((state) => state.team.currentTeam);
+
+  const handleSelect = (team) => {
+    dispatch(setTeam(team));
+  };
+
+  return (
+    <div className="bg-indigo-500">
+      <Listbox value={selectedTeam} onChange={handleSelect}>
+        <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+          <span className="block truncate">
+            {selectedTeam ? selectedTeam.name : "Choose a Team"}
+          </span>
+        </Listbox.Button>
+        <Transition
+          as={Fragment}
+          leave="transition ease-in duration-100"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-orange py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+            {teams.map((team, i) => (
+              <Listbox.Option
+                key={i}
+                className={({ active }) =>
+                  `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                    active ? `bg-amber-100 text-amber-900` : `text-gray-900`
+                  }`
+                }
+                value={team}
+              >
+                {({ selected }) => (
+                  <>
+                    <span
+                      className={`block truncate ${
+                        selected ? "font-medium" : "font-normal"
+                      }`}
+                    >
+                      {team.name}
+                    </span>
+                    {selected ? (
+                      <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                        <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                      </span>
+                    ) : null}
+                  </>
+                )}
+              </Listbox.Option>
+            ))}
+          </Listbox.Options>
+        </Transition>
+      </Listbox>
+    </div>
+  );
+};
+
+export default TeamDropdown;
--- frontend-vite/src/pages/Home.jsx
+++ frontend-vite/src/pages/Home.jsx
@@ -1,14 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { getAllTeams } from "../util/apiCalls";
+import TeamDropdown from "../components/TeamDropdown";
 
 const Home = () => {
   const [teams, setTeams] = useState();
 
   useEffect(() => {
-    setTeams(getAllTeams());
+    getAllTeams()
+      .then((res) => setTeams(res))
+      .catch((err) => console.log(err))
+      .finally(console.log("finally"));
   }, []);
 
-  return <div>Home</div>;
+  return (
+    <div className="h-screen w-screen bg-slate-400">
+      {teams && <TeamDropdown teams={teams} />}
+    </div>
+  );
 };
 
 export default Home;
