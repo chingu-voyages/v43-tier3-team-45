1993f2ed933b2d9240c303099c3436f9a873cc78
BillyMiles <williammiles201@gmail.com>
2023-04-07 11:44:16 -0400

    switching branch

--- frontend-vite/src/components/CreateIssueModal.jsx
+++ frontend-vite/src/components/CreateIssueModal.jsx
@@ -50,11 +50,11 @@ const CreateIssueModal = ({setShowing}) => {
   }
 
   const handlePriority = (priority) => {
-    setPriority(priority)
+        setPriority(priority)
   }
 
   const handleType = (type) => {
-      setType(type)
+        setType(type)
   };
 
   const handleSave = (e) => {
@@ -65,147 +65,147 @@ const CreateIssueModal = ({setShowing}) => {
   }
   
   const postIssue = async (testIssue) => {
-      try {
-        const response = await axios.post(`http://localhost:8080/api/issues/create?projectId=641ba8e494ba927d1a1e932d`, testIssue);
-        return response.data;
-      } catch (error) {
-        console.log("error", error)
-      }
+        try {
+          const response = await axios.post(`http://localhost:8080/api/issues/create?projectId=641ba8e494ba927d1a1e932d`, testIssue);
+          return response.data;
+        } catch (error) {
+          console.log("error", error)
+        }
     }
 
     return (
-    <div className="fixed z-10 inset-0 overflow-y-auto">
-    <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-      <div className="fixed inset-0 transition-opacity" aria-hidden="true">
-        <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
-      </div>
-      <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
-      <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-             <form className="mt-6">
-
-           <div className="mb-2">
-             <label>
-              <span className="text-gray-700">{useSelector(state => state.user.email)}</span>
-               <input
-                type="text"
-                name="name"
-                className="
-                  w-full
-                  block px-16 py-2 mt-2
-                  border-gray-300
-                  rounded-md
-                  shadow-sm
-                  focus:border-indigo-300
-                  focus:ring
-                  focus:ring-indigo-200
-                  focus:ring-opacity-50
-                "
-                placeholder="Title"
-                onChange={handleTitle}
-              />
-            </label>
+        <div className="fixed z-10 inset-0 overflow-y-auto">
+        <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+          <div className="fixed inset-0 transition-opacity" aria-hidden="true">
+            <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
           </div>
-          <div>
-                <TypeDropdown handleType={handleType} />
+          <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+          <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                <form className="mt-6">
+
+              <div className="mb-2">
+                <label>
+                  <span className="text-gray-700">{useSelector(state => state.user.email)}</span>
+                  <input
+                    type="text"
+                    name="name"
+                    className="
+                      w-full
+                      block px-16 py-2 mt-2
+                      border-gray-300
+                      rounded-md
+                      shadow-sm
+                      focus:border-indigo-300
+                      focus:ring
+                      focus:ring-indigo-200
+                      focus:ring-opacity-50
+                    "
+                    placeholder="Title"
+                    onChange={handleTitle}
+                  />
+                </label>
+              </div>
+              <div>
+                    <TypeDropdown handleType={handleType} />
+                </div>
+                <div>
+                    <PriorityDropdown handlePriority={handlePriority}/>
+                </div>
+
+              <div className="mb-2">
+                <label>
+                  <span class="text-gray-700">Description</span>
+                  <textarea
+                    name="message"
+                    className="
+                        block
+                        w-full
+                        mt-2 px-16 py-8
+                        border-gray-300
+                        rounded-md
+                        shadow-sm
+                        focus:border-indigo-300
+                        focus:ring
+                        focus:ring-indigo-200
+                        focus:ring-opacity-50
+                      "
+                      rows="5"
+                    onChange={handleDescription}
+                  ></textarea>
+                </label>
+              </div>
+
+              <div className="mb-2">
+                <label>
+                  <span class="text-gray-700">Comment</span>
+                  <textarea
+                    name="message"
+                    className="
+                        block
+                        w-full
+                        mt-2 px-16 py-8
+                        border-gray-300
+                        rounded-md
+                        shadow-sm
+                        focus:border-indigo-300
+                        focus:ring
+                        focus:ring-indigo-200
+                        focus:ring-opacity-50
+                      "
+                    rows="5"
+                    onChange={handleComment}
+                  ></textarea>
+                </label>
+              </div>
+
+              <div class="mb-6">
+                <button
+                  type="submit"
+                  className="
+                      h-10
+                      px-5
+                      text-indigo-100
+                      bg-indigo-700
+                      rounded-lg
+                      transition-colors
+                      duration-150
+                      focus:shadow-outline
+                      hover:bg-indigo-800
+                    "
+                    onClick={handleSave}
+                >
+                  Save
+                </button>
+              </div>
+
+              <div class="mb-6">
+                <button
+                    type="submit"
+                    className="
+                      h-10
+                      px-5
+                      text-indigo-100
+                      bg-indigo-700
+                      rounded-lg
+                      transition-colors
+                      duration-150
+                      focus:shadow-outline
+                      hover:bg-indigo-800
+                      "
+                  onClick={(setShowing) => setShowing()}
+                >
+                  Cancel
+                </button>
+              </div>
+
+              <div></div>
+            </form>
+
+            <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
             </div>
-            <div>
-                <PriorityDropdown handlePriority={handlePriority}/>
-            </div>
-
-          <div className="mb-2">
-            <label>
-              <span class="text-gray-700">Description</span>
-              <textarea
-                name="message"
-                className="
-                    block
-                    w-full
-                    mt-2 px-16 py-8
-                    border-gray-300
-                    rounded-md
-                    shadow-sm
-                    focus:border-indigo-300
-                    focus:ring
-                    focus:ring-indigo-200
-                    focus:ring-opacity-50
-                  "
-                  rows="5"
-                onChange={handleDescription}
-              ></textarea>
-            </label>
           </div>
-
-          <div className="mb-2">
-            <label>
-              <span class="text-gray-700">Comment</span>
-              <textarea
-                name="message"
-                className="
-                    block
-                    w-full
-                    mt-2 px-16 py-8
-                    border-gray-300
-                    rounded-md
-                    shadow-sm
-                    focus:border-indigo-300
-                    focus:ring
-                    focus:ring-indigo-200
-                    focus:ring-opacity-50
-                  "
-                rows="5"
-                onChange={handleComment}
-              ></textarea>
-            </label>
-          </div>
-
-          <div class="mb-6">
-            <button
-              type="submit"
-              className="
-                  h-10
-                  px-5
-                  text-indigo-100
-                  bg-indigo-700
-                  rounded-lg
-                  transition-colors
-                  duration-150
-                  focus:shadow-outline
-                  hover:bg-indigo-800
-                "
-                onClick={handleSave}
-            >
-              Save
-            </button>
-          </div>
-
-          <div class="mb-6">
-            <button
-                type="submit"
-                className="
-                  h-10
-                  px-5
-                  text-indigo-100
-                  bg-indigo-700
-                  rounded-lg
-                  transition-colors
-                  duration-150
-                  focus:shadow-outline
-                  hover:bg-indigo-800
-                  "
-              onClick={(setShowing) => setShowing()}
-            >
-              Cancel
-            </button>
-          </div>
-
-          <div></div>
-        </form>
-
-        <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
         </div>
       </div>
-    </div>
-  </div>
 )
 
 };
--- frontend-vite/src/pages/CreateIssue.jsx
+++ frontend-vite/src/pages/CreateIssue.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, Fragment } from "react"
 import CreateIssueModal from '../components/CreateIssueModal'
 import { Transition } from '@headlessui/react'
 
