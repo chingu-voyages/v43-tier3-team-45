522f6d6d0aa3fc0798260101a2f62dd12c8d9992
Shawn Koong <shawnkoong@gmail.com>
2023-04-11 16:46:11 -0700

    adding preventDefault to teamMemberDropdown button to prevent page refresh

--- frontend-vite/src/components/TeamMemberDropdown.jsx
+++ frontend-vite/src/components/TeamMemberDropdown.jsx
@@ -2,13 +2,15 @@ import React, { Fragment, useEffect } from "react";
 import { Popover, Transition } from "@headlessui/react";
 import { useDispatch, useSelector } from "react-redux";
 import { addMemberToSelectedList } from "../store/teamReducer";
+import Avatar from "./Avatar";
 
 const TeamMemberDropdown = () => {
   const dispatch = useDispatch();
   const selectedList = useSelector((state) => state.team.selectedList);
   const filteredList = useSelector((state) => state.team.filteredList);
 
-  const handleClick = (member) => {
+  const handleClick = (e, member) => {
+    e.preventDefault();
     dispatch(addMemberToSelectedList(member.id));
     console.log({ selectedList });
     console.log({ filteredList });
@@ -16,7 +18,9 @@ const TeamMemberDropdown = () => {
 
   return (
     <Popover className="relative">
-      <Popover.Button>Add member</Popover.Button>
+      <Popover.Button>
+        <span>Add member</span>
+      </Popover.Button>
       <Transition
         as={Fragment}
         enter="transition ease-out duration-200"
@@ -27,13 +31,24 @@ const TeamMemberDropdown = () => {
         leaveTo="opacity-0 translate-y-1"
       >
         <Popover.Panel className="absolute z-10">
-          {filteredList &&
-            filteredList.map((member) => (
-              <button className="outline" onClick={(e) => handleClick(member)}>
-                <img src={member.avatarUrl} alt={"avatar"} />
-                <div>{member.firstName}</div>
-              </button>
-            ))}
+          <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+            <div className="relative grid gap-8 bg-white p-7 grid-cols-1">
+              {filteredList &&
+                filteredList.map((member) => (
+                  <button
+                    className="flex rounded-lg outline"
+                    onClick={(e) => handleClick(e, member)}
+                  >
+                    <Avatar
+                      src={member.avatarUrl}
+                      alt={member.firstName}
+                      size={12}
+                    />
+                    <p className="text-sky-400">{member.firstName}</p>
+                  </button>
+                ))}
+            </div>
+          </div>
         </Popover.Panel>
       </Transition>
     </Popover>
