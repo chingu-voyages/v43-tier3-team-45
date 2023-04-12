159adff6e3975e73249e8801c9ff74d7b43a62a4
Shawn Koong <shawnkoong@gmail.com>
2023-04-12 15:34:33 -0700

    making entire task card clickable

--- frontend-vite/src/components/Task.jsx
+++ frontend-vite/src/components/Task.jsx
@@ -8,43 +8,40 @@ export default function Task({ task, index }) {
 
   return (
     <>
-      <Draggable draggableId={`${task.id}`} key={task.id} index={index}>
-        {(provided, snapshot) => (
-          <div
-            {...provided.draggableProps}
-            {...provided.dragHandleProps}
-            ref={provided.innerRef}
-            isDragging={snapshot.isDragging}
-            style={{
-              userSelect: "none",
-              padding: 16,
-              margin: "0 00 8px 0",
-              minHeight: "50px",
-              backgroundColor: snapshot.isDragging ? "#263B4A" : "#456C86",
-              color: "white",
-              ...provided.draggableProps.style,
-            }}
-          >
-            <div class="border m-2 p-2">
-              <div id="task-title">{task.title}</div>
-              <div id="task-status">
-                <span> Status: {task.status}</span>
+      <div onClick={() => setIsShowing((isShowing) => !isShowing)}>
+        <Draggable draggableId={`${task.id}`} key={task.id} index={index}>
+          {(provided, snapshot) => (
+            <div
+              {...provided.draggableProps}
+              {...provided.dragHandleProps}
+              ref={provided.innerRef}
+              isDragging={snapshot.isDragging}
+              style={{
+                userSelect: "none",
+                padding: 16,
+                margin: "0 00 8px 0",
+                minHeight: "50px",
+                backgroundColor: snapshot.isDragging ? "#263B4A" : "#456C86",
+                color: "white",
+                ...provided.draggableProps.style,
+              }}
+            >
+              <div class="border m-2 p-2">
+                <div id="task-title">{task.title}</div>
+                <div id="task-status">
+                  <span> Status: {task.status}</span>
+                </div>
+                <div id="task-priority">
+                  <span>Priority: {task.priority}</span>
+                </div>
+                <div>{task.id}</div>
+                <div id="user-avatar"></div>
               </div>
-              <div id="task-priority">
-                <span>Priority: {task.priority}</span>
-              </div>
-              <div>{task.id}</div>
-              <div id="user-avatar"></div>
-              <div>
-                <button onClick={() => setIsShowing((isShowing) => !isShowing)}>
-                  Edit
-                </button>
-              </div>
+              {provided.placeholder}
             </div>
-            {provided.placeholder}
-          </div>
-        )}
-      </Draggable>
+          )}
+        </Draggable>
+      </div>
       <div>
         <Transition
           show={isShowing}
