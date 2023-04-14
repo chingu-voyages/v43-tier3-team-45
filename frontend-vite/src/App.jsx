3774bdb098d1b1b13005d523465ccad22629eb9d
talarsen <talarsen74@gmail.com>
2023-04-13 21:06:40 -0700

    remove navBar route and extra /

--- frontend-vite/src/App.jsx
+++ frontend-vite/src/App.jsx
@@ -13,8 +13,7 @@ function App() {
   return (
     <Routes>
       <Route exact path="/" element={<Login />} />
-      <Route exact path="//register" element={<CreateProfile />} />
-      <Route exact path="/navbar" element={<NavBar />} />
+      <Route exact path="/register" element={<CreateProfile />} />
       <Route element={<PrivateRoutes />}>
         <Route exact path="/home" element={<Home />} />
         <Route exact path="/profile" element={<Profile />} />
