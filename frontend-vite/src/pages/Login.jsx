2f203ecadb814e13337b57e8b353b85ef8875872
Shawn Koong <shawnkoong@gmail.com>
2023-04-16 16:56:44 -0700

    adding image on the side

--- frontend-vite/src/pages/Login.jsx
+++ frontend-vite/src/pages/Login.jsx
@@ -2,8 +2,17 @@ import LoginForm from "../components/LoginForm";
 
 function Login() {
   return (
-    <div>
-      <LoginForm />
+    <div className="grid grid-cols-1 md:grid-cols-2 h-screen">
+      <div className="h-full">
+        <img
+          className="h-full"
+          src="https://chinguboard-dev.s3.us-east-2.amazonaws.com/kanban.jpg"
+          alt="kanban"
+        />
+      </div>
+      <div>
+        <LoginForm />
+      </div>
     </div>
   );
 }
