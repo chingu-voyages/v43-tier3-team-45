0a20591f31216341fd33216bbaf8633854a27042
Shawn Koong <shawnkoong@gmail.com>
2023-04-17 12:55:38 -0700

    adding create team button to navbar

--- frontend-vite/src/components/NavBar.jsx
+++ frontend-vite/src/components/NavBar.jsx
@@ -39,49 +39,63 @@ const NavBar = () => {
 
   return (
     <nav
-      className="grid grid-cols-3 gap-none
+      className="grid grid-cols-5 gap-none
      items-center px-5 mx-auto bg-white-100"
     >
-      <div className="font-sans text-2xl text-blue-500 font-bold ml-3 w-48">
-        Chingu Board
-      </div>
-      {/* <button
-          className="cursor-pointer rounded-lg bg-indigo-300 py-2 shadow-md"
-          onClick={(e) => handleCreateTeam(e)}
-        >
-          <span className="block text-black">Create Team</span>
-        </button> */}
-      <div className="flex grid-cols-5 gap-1 items-center">
-        <div>{teams && <TeamDropdown teams={teams} />}</div>
-        <div className="flex p-3">
-          {teamMembers &&
-            teamAvatars.map((member, index) => (
-              <Avatar
-                key={index}
-                size={12}
-                src={member.avatarUrl}
-                alt="team member"
-              />
-            ))}
-          {hiddenAvatarsCount > 0 && (
-            <div className="rounded-full w-12 h-12 text-center pt-3 text-xs border border-blue-200">{`+${hiddenAvatarsCount}more`}</div>
-          )}
+      <div className="flex justify-start">
+        <div className="font-sans text-2xl text-blue-500 ml-2 font-bold">
+          Chingu Board
         </div>
       </div>
 
-      <div className="grid grid-cols-2 gap-5 w-60 items-center px-6 ml-56">
-        <div className="border-2 border-blue-500 rounded-none p-2 w-16">
-          <button onClick={(e) => handleLogout(e)}>Logout</button>
+      <div className="col-span-3">
+        <div className="grid grid-cols-4 gap-1">
+          <div className="flex justify-end items-center mr-3">
+            <button
+              className="cursor-pointer rounded-lg bg-indigo-300 py-2 shadow-md"
+              onClick={(e) => handleCreateTeam(e)}
+            >
+              <span className="block text-black text-sm px-2">Create Team</span>
+            </button>
+          </div>
+          <div className="flex items-center col-span-3">
+            <div>{teams && <TeamDropdown teams={teams} />}</div>
+            <div className="flex p-3">
+              {teamMembers &&
+                teamAvatars.map((member, index) => (
+                  <Avatar
+                    key={index}
+                    size={12}
+                    src={member.avatarUrl}
+                    alt="team member"
+                  />
+                ))}
+              {hiddenAvatarsCount > 0 && (
+                <div className="flex items-center justify-center rounded-full w-12 h-12 text-sm font-semibold border-2 border-blue-400 bg-indigo-200 text-gray-600">{`+ ${hiddenAvatarsCount}`}</div>
+              )}
+            </div>
+          </div>
         </div>
-        <div>
+      </div>
+
+      <div className="flex justify-end">
+        <div className="grid grid-cols-2 gap-2 items-center">
           <button
-            className="justify-self items-center"
-            onClick={() => {
-              navigate("/profile");
-            }}
+            className="rounded-lg bg-indigo-300 py-2 shadow-md cursor-pointer px-2 text-sm"
+            onClick={(e) => handleLogout(e)}
           >
-            <Avatar size={12} src={user.avatarUrl} alt={"user"} />
+            Logout
           </button>
+          <div>
+            <button
+              className="justify-self items-center"
+              onClick={() => {
+                navigate("/profile");
+              }}
+            >
+              <Avatar size={12} src={user.avatarUrl} alt={"user"} />
+            </button>
+          </div>
         </div>
       </div>
     </nav>
