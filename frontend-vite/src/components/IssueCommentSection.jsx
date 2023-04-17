b9cafede6dd2a91d817ee47f1c883ad50e252e1a
BillyMiles <williammiles201@gmail.com>
2023-04-16 20:59:22 -0400

    added avatar to comment section

--- frontend-vite/src/components/IssueCommentSection.jsx
+++ frontend-vite/src/components/IssueCommentSection.jsx
@@ -1,9 +1,19 @@
-
+import Avatar from "./Avatar"
 export const IssueCommentSection = ( {data} ) => {
+console.log("data ICS", data.createdBy.avatarUrl)
 
+    let user = data.createdBy
+    
     return (
         <div>
-         {data.text}, {data.createdBy.email}
+            <Avatar 
+            src={user.avatarUrl}
+            alt={user.firstName}
+            size={12}
+            />
+            <div>
+            {data.text}
+            </div>
         </div>
     )
 
